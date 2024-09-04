0a4d0937ffaaa650caae9eea40030ae748b6e894
emrahaydemir <firstlord97@gmail.com>
2024-09-04 21:46:07 +0300

    basic rag

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from typing import List
 from langchain_core.runnables import RunnableLambda
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.prompts import ChatMessagePromptTemplate
+
 
 load_dotenv()
 
@@ -35,14 +39,23 @@ documents = [  # vector documentleri olustur. bunlar dosyalar da olabilir, metad
 
 vectorstore = Chroma.from_documents(documents=documents, embedding=OpenAIEmbeddings())
 
+retriever = RunnableLambda(vectorstore.similarity_search).bind(
+    k=1
+)  # k=1 vector sonuclarinda ilk eslesen sonucu kullanmasi icin. digerini yoksayar
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+message = """
+Answer this question using the provided context only.
+{question}
+
+Context: {context}
+"""
+
+prompt = ChatPromptTemplate.from_messages([("human", message)])
+
+chain = {"context": retriever, "question": RunnablePassthrough()} | prompt | llm
+
 if __name__ == "__main__":
-    embedding = OpenAIEmbeddings().embed_query(
-        "dog"
-    )  # "dog" querysine sahip bir vector olusturduk
-    print(
-        vectorstore.similarity_search_by_vector(embedding=embedding)
-    )  # olusturulan vectoru, vectorstore uzerinden similarity aramasinda kullandik.
-    print(vectorstore.similarity_search("cat"))  # kelime bazli arama
-    print(
-        vectorstore.similarity_search_with_score("rabbit")
-    )  # burada kelime bazli arama yapilir, ekstra olarak eslesme skorlari da sonuclara eklenir
+    response = chain.invoke("tell me about cats")
+    print(response.content)
